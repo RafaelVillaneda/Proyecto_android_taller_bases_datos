53f23b076a72a56aa10e79cbd69889fab2cfda3f
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2021-11-20 13:39:51 -0600

    Se agrego la pantalla de bajas

---
+++ app/src/main/java/com/example/proyecto_android/Activity_Bajas.java
@@ -0,0 +1,15 @@
+package com.example.proyecto_android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_Bajas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bajas_clientes);
+    }
+
+}
--- app/src/main/java/com/example/proyecto_android/MainActivity.java
+++ app/src/main/java/com/example/proyecto_android/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void btn_ingresar(View a){
-        Intent i=new Intent(this,Activity_Altas.class);
+        Intent i=new Intent(this,Activity_Bajas.class);
         startActivity(i);
     }
 }
