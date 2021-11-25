8c259b4651b3eb8ea0be2513d5a8bacd0316a3f0
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2021-11-24 19:59:06 -0600

    Se termino de agregar la pantalla de cambios

--- app/src/main/java/com/example/proyecto_android/Activity_Cambios.java
+++ app/src/main/java/com/example/proyecto_android/Activity_Cambios.java
@@ -1,18 +1,17 @@
 package com.example.proyecto_android;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
 
-public class Activity_Altas extends AppCompatActivity {
+public class Activity_Cambios extends Activity {
     EditText caja_indentificador,caja_nombre_conpañia,caja_nombre_contacto,caja_titulo_contacto,caja_direccion,caja_ciudad,caja_region,caja_codigo_postal,caja_pais,caja_telefono,caja_fax;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.altas_clientes);
+        setContentView(R.layout.cambios_clientes);
         caja_indentificador=findViewById(R.id.caja_indentificador_cambios);
         caja_nombre_conpañia=findViewById(R.id.caja_nombre_compañia_cambios);
         caja_nombre_contacto=findViewById(R.id.caja_nombre_contacto_cambios);
@@ -25,22 +24,4 @@ public class Activity_Altas extends AppCompatActivity {
         caja_telefono=findViewById(R.id.caja_telefono_cambios);
         caja_fax=findViewById(R.id.caja_fax_cambios);
     }
-    public boolean verificarcajasVacias(){
-        if(caja_indentificador.getText().toString().isEmpty()|| caja_nombre_conpañia.getText().toString().isEmpty()){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public void agregarCliente(View v){
-            if(verificarcajasVacias()==false){
-                //codigo de agregar
-            }else{
-                Toast.makeText(getBaseContext(),"No puede estar vacio el indentificador y/o nombre de compañia",Toast.LENGTH_LONG).show();
-            }
-    }
-
-
-
 }
