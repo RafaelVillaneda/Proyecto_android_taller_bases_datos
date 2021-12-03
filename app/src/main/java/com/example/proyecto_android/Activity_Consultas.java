7e0bb169849b59b62cd54400efaf9f945cca794c
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2021-12-03 16:49:06 -0600

    Se implemento una mejor buqueda en la pantalla de consultas

--- app/src/main/java/com/example/proyecto_android/Activity_Consultas.java
+++ app/src/main/java/com/example/proyecto_android/Activity_Consultas.java
@@ -2,6 +2,8 @@ package com.example.proyecto_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,68 +37,79 @@ public class Activity_Consultas extends Activity {
         setContentView(R.layout.activity_consultas);
         id=findViewById(R.id.caja_consultas);
         recicler=findViewById(R.id.reciclerView);
+        recicler.setHasFixedSize(true);
+
+        layoutManager=new LinearLayoutManager(this);
+
+        recicler.setLayoutManager(layoutManager);
+        id.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String[] datos = {""};
+                int[] c = new int[1];
+
+                if(id.getText().toString().isEmpty()){
+                    //---------------------Recycler View
+
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            northwindBD conexion=northwindBD.gettAppDatabase(getBaseContext());
+                            e= conexion.clienteDAO().optenerTodos();
+                            System.out.println("Hola");
+                            //System.out.println("Tamaño----------------->"+e.size());
+                            c[0] =e.size();
+                            for (clientes a:e) {
+                                Log.d("datos->",a.toString());
+                            }
+                            for(int i=0;i<c[0];i++){
+                                datos[0] = datos[0]+e.get(i)+"/";
+                            }
+                            //System.out.println(Arrays.toString(datos));
+
+                            //System.out.println("Tamaño----------------->"+e);
+                            adaper=new AdaptadorRegistros(datos[0].split("/"));
+                            recicler.setAdapter(adaper);
+
+                        }
+                    }).start();
+
+
+                }else{
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            northwindBD conexion=northwindBD.gettAppDatabase(getBaseContext());
+                            e= conexion.clienteDAO().optenerPorFiltrado("%"+id.getText().toString()+"%");
+                            System.out.println("Hola");
+                            //System.out.println("Tamaño----------------->"+e.size());
+                            c[0] =e.size();
+                            for(int i=0;i<c[0];i++){
+                                datos[0] = datos[0]+e.get(i)+"/";
+                            }
+                            adaper=new AdaptadorRegistros(datos[0].split("/"));
+                            recicler.setAdapter(adaper);
+                        }
+                    }).start();
+                }
+            }
+        });
     }
 
 
     public void buscar(View v){
-        String[] datos = {""};
-        int[] c = new int[1];
-        if(id.getText().toString().isEmpty()){
-            //---------------------Recycler View
-            recicler.setHasFixedSize(true);
 
-            layoutManager=new LinearLayoutManager(this);
-
-            recicler.setLayoutManager(layoutManager);
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    northwindBD conexion=northwindBD.gettAppDatabase(getBaseContext());
-                    e= conexion.clienteDAO().optenerTodos();
-                    System.out.println("Hola");
-                    //System.out.println("Tamaño----------------->"+e.size());
-                    c[0] =e.size();
-                    for (clientes a:e) {
-                        Log.d("datos->",a.toString());
-                    }
-                    for(int i=0;i<c[0];i++){
-                        datos[0] = datos[0]+e.get(i)+"/";
-                    }
-                    //System.out.println(Arrays.toString(datos));
-
-                    //System.out.println("Tamaño----------------->"+e);
-                    adaper=new AdaptadorRegistros(datos[0].split("/"));
-                    recicler.setAdapter(adaper);
-
-                }
-            }).start();
-
-
-        }else {
-            //---------------------Recycler View
-            recicler.setHasFixedSize(true);
-
-            layoutManager=new LinearLayoutManager(this);
-
-            recicler.setLayoutManager(layoutManager);
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    northwindBD conexion=northwindBD.gettAppDatabase(getBaseContext());
-                    e= conexion.clienteDAO().optenerPorFiltrado("%"+id.getText().toString()+"%");
-                    System.out.println("Hola");
-                    //System.out.println("Tamaño----------------->"+e.size());
-                    c[0] =e.size();
-                    for(int i=0;i<c[0];i++){
-                        datos[0] = datos[0]+e.get(i)+"/";
-                    }
-                    adaper=new AdaptadorRegistros(datos[0].split("/"));
-                    recicler.setAdapter(adaper);
-                }
-            }).start();
-        }
     }
 
 
